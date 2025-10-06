864fee629c9e6f8cd54aec055b92f3784b6d9fb6
Arman Mir <mirarman8583@gmail.com>
2025-10-06 10:57:05 +0600

    Params UseParams Hook and fetching data dynamically with id

--- boi-poka/src/Pages/Book/Book.jsx
+++ boi-poka/src/Pages/Book/Book.jsx
@@ -9,38 +9,40 @@ const Book = ({ singleBook }) => {
     const { image, bookName, author, rating, category, yearOfPublishing, tags, bookId } = singleBook;
     // const data = use(bookPromise);
 
-    console.log(singleBook);
+    // console.log(singleBook);
 
     return (
         <Link to={`/bookDetails/${bookId}`}>
-        <div className="card bg-base-100 w-96 shadow-sm border p-6 ">
-            <figure className="p-4 bg-gray-100 w-2/3 mx-auto rounded-xl" >
-                <img className="h-[166px]"
-                    src={image}
-                    alt="Shoes" />
-            </figure>
+            <div className="card bg-base-100 w-96 shadow-sm border p-6 ">
+                <figure className="p-4 bg-gray-100 w-2/3 mx-auto rounded-xl" >
+                    <img className="h-[166px]"
+                        src={image}
+                        alt="Shoes" />
+                </figure>
 
-            <div className="card-body">
-                <div className="flex justify-between " >
-                    {
-                        tags.map(tag => <button className="" >{tag}</button>)
-                    }
-                </div>
-                <h2 className="card-title">
-                    {bookName}
-                    <div className="badge badge-secondary">{yearOfPublishing}</div>
-                </h2>
-                <p>  By : {author}</p>
-                <div className="border-t border-dashed border-pink-300" >
+                <div className="card-body">
+                    <div className="flex justify-between " >
+                        {
+                            tags.map((tag, index) => {
+                                return <button key={index} className="">{tag}</button>;
+                            })
+                        }
+                    </div>
+                    <h2 className="card-title">
+                        {bookName}
+                        <div className="badge badge-secondary">{yearOfPublishing}</div>
+                    </h2>
+                    <p>  By : {author}</p>
+                    <div className="border-t border-dashed border-pink-300" >
 
-                </div>
-                <div className="card-actions justify-between">
-                    <div className="badge badge-outline">{category}</div>
-                    <div className="badge badge-outline">{rating}<FaStarHalfAlt />
+                    </div>
+                    <div className="card-actions justify-between">
+                        <div className="badge badge-outline">{category}</div>
+                        <div className="badge badge-outline">{rating}<FaStarHalfAlt />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </Link>
     );
 };
--- boi-poka/src/Pages/BookDetails/BookDetails.jsx
+++ boi-poka/src/Pages/BookDetails/BookDetails.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+
+    const { id } = useParams();
+    const data = useLoaderData();
+    const bookID = parseInt(id)
+
+    const singleBook = data.find(book => book.bookId === bookID)
+
+
+    const { bookName, image, } = singleBook;
+
+
+
     return (
-        <div>
-            <h1>Book dEtails</h1>
+        <div className=' w-2/3 mx-auto' >
+            <h1>
+                <img className='w-48' src={image} alt="" />
+                <p>{bookName}</p>
+
+                <button className="btn btn-accent m-2">Read</button>
+                <button className="btn btn-info m-2">WishList</button>
+
+            </h1>
         </div>
     );
 };
