045d2c22fe07af228e10768f7e3fb967b29ca085
Arman Mir <mirarman8583@gmail.com>
2025-10-06 08:30:27 +0600

    Polishing The PRoduct Card and adding react icon

--- boi-poka/src/Pages/Book/Book.jsx
+++ boi-poka/src/Pages/Book/Book.jsx
@@ -1,29 +1,36 @@
 // import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
+
 
 const Book = ({ singleBook }) => {
 
 
-    const {image, bookName, author} = singleBook;
+    const { image, bookName, author, rating, category } = singleBook;
     // const data = use(bookPromise);
 
     // console.log(singleBook);
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure className="p-3" >
-                <img
+        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+            <figure className="p-4 bg-gray-100 w-2/3 mx-auto rounded-xl" >
+                <img className="h-[166px]"
                     src={image}
                     alt="Shoes" />
             </figure>
+            <div className=" ml-8 mt-3 flex gap-10 text-green-600" >
+                <p>Young Adult</p>
+                <p>Identity</p>
+            </div>
             <div className="card-body">
                 <h2 className="card-title">
                     {bookName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
-                <p>{author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                <p>  By : {author}</p>
+                <div className="card-actions justify-between">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating}<FaStarHalfAlt />
+                    </div>
                 </div>
             </div>
         </div>
--- boi-poka/src/Pages/Books/Books.jsx
+++ boi-poka/src/Pages/Books/Books.jsx
@@ -23,7 +23,7 @@ const Books = ({data}) => {
 
             <Suspense fallback={<span className="loading loading-spinner text-error"></span>
             }>
-               <div className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4' >
+               <div className='grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-6' >
                 {
                 data.map((singleBook)=> <Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
                }
